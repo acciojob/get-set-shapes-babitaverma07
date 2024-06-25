a786f80bc96b60876ff5b38c00c30db7ad3aae3d
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-06-25 12:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-//complete this code
-class Rectangle {}
+// Rectangle class
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  // Getter for width
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+  // Getter for height
+  get height() {
+    return this._height;
+  }
+
+  // Method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+// Square class extending Rectangle
+class
