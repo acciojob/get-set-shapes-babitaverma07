f35384a7eed257f66aecf4149da7c9fb2e703a11
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 13:55:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,67 @@
+// Rectangle class
 class Rectangle {
-    constructor(width, height) {
-        this._width = width;
-        this._height = height;
-    }
-    get width() {
-        return this._width;
-    }
-    get height() {
-        return this._height;
-    }
-    getArea() {
-        return this._width * this._height;
-    }
+  /**
+   * Constructor method
+   * @param {number} width - The width of the rectangle
+   * @param {number} height - The height of the rectangle
+   */
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  /**
+   * Getter method for width
+   * @returns {number} The width of the rectangle
+   */
+  get width() {
+    return this._width;
+  }
+
+  /**
+   * Getter method for height
+   * @returns {number} The height of the rectangle
+   */
+  get height() {
+    return this._height;
+  }
+
+  /**
+   * Method to calculate the area of the rectangle
+   * @returns {number} The area of the rectangle
+   */
+  getArea() {
+    return this.width * this.height;
+  }
 }
+
+// Square class extends Rectangle
 class Square extends Rectangle {
-    constructor(side) {
-        super(side, side); // Since a square has equal sides, both width and height are set to side
-    }
-    getPerimeter() {
-        return 4  this.width; // Perimeter of a square: 4  side
-    }
+  /**
+   * Constructor method
+   * @param {number} side - The side length of the square
+   */
+  constructor(side) {
+    super(side, side); // Call Rectangle constructor with side as both width and height
+  }
+
+  /**
+   * Method to calculate the perimeter of the square
+   * @returns {number} The perimeter of the square
+   */
+  getPerimeter() {
+    return 4 * this.width; // Since all sides are equal, 4 times the width is the perimeter
+  }
 }
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+
+// Example usage
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); // Output: 5
+console.log(rectangle.height); // Output: 10
+console.log(rectangle.getArea()); // Output: 50
+
+const square = new Square(7);
+console.log(square.width); // Output: 7
+console.log(square.height); // Output: 7
+console.log(square.getArea()); // Output: 49
+console.log(square.getPerimeter()); // Output: 28
